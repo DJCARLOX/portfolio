8ef92b0772560b1d2bad953c36c835fbfd680567
Carlos <cjgl2004@gmail.com>
2023-12-05 02:03:19 +0100

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,33 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+    res.render("index", { title: "PORTFOLIO" });
+});
+
+router.get("/museo", (req, res) => {
+    res.render("museo", { title: "Museo Virtual" });
+});
+
+router.get("/hugo", (req, res) => {
+    res.render("hugo", { title: "Hugo Framework" });
+});
+
+router.get("/arquitectura", (req, res) => {
+    res.render("arquitectura", { title: "Arquitectura Web" });
+});
+
+router.get("/servidores", (req, res) => {
+    res.render("servidores", { title: "Servidores Web" });
+});
+
+router.get("/nodejs", (req, res) => {
+    res.render("nodejs", { title: "Nodejs" });
+});
+
+router.get("/general", (req, res) => {
+    res.render("general", { title: "En General" });
+});
+
+export default router;
